db0d59b8d45193fd2bbae676c0b7163c3903b346
dkavanagh <dkavanagh@gmail.com>
2017-07-25 23:17:21 -0700

    setup.py to aid in deployment

---
+++ setup.py
@@ -0,0 +1,31 @@
+
+import os.path
+
+from setuptools import setup, find_packages
+
+
+__version__ = 'devel'
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+
+requires = [
+    'flask',
+]
+
+setup(
+    name='pybowieconsole',
+    version=__version__,
+    description='Web console for Bowie',
+    long_description=README,
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    author='David Kavanagh',
+    author_email='dkavanagh@gmail.com',
+    url='',
+    keywords='',
+    packages=find_packages()
+)
