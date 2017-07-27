2a4bbac3a6d52542f9d9e98d4d68f686eb263318
dkavanagh <dkavanagh@gmail.com>
2017-07-26 21:59:21 -0700

    added material design and colored buttons

--- pybowieconsole/js/bowie.js
+++ pybowieconsole/js/bowie.js
@@ -1,4 +1,9 @@
-angular.module('bowie', [])
+angular.module('bowie', ['ngMaterial'])
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue')
+    .accentPalette('orange');
+})
 .factory('ActionService', ['$http', '$interpolate', function($http, $interpolate) {
     $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
     return {
@@ -23,8 +28,9 @@ angular.module('bowie', [])
 }])
 .directive('lightswitch', [function() {
     return {
-        template: '<input type="checkbox" ng-model="value", ng-click="clicked()"/>',
+        template: '<md-switch ng-model="value" ng-click="clicked()"><ng-transclude></md-switch>',
         restrict: 'A',
+        transclude: true,
         controller: ['$scope', 'ActionService', function($scope, ActionService) {
             $scope.value = false;
             $scope.clicked = function() {
@@ -32,6 +38,23 @@ angular.module('bowie', [])
                 ActionService.performAction('#', 'Q', '1', ($scope.value)?'255':'0', 'Q', '2', ($scope.value)?'255':'0');
             };
         }]
+    };
+}])
+.controller('bowieCtl', ['$scope', 'ActionService', function($scope, ActionService) {
+    $scope.redClicked = function() {
+        ActionService.performAction('#', 'P', '1', '1', 'P', '0', '0');
+    };
+    $scope.greenClicked = function() {
+        ActionService.performAction('#', 'G', '1', '1', 'G', '0', '0');
+    };
+    $scope.yellowClicked = function() {
+        ActionService.performAction('#', 'Y', '1', '1', 'Y', '0', '0');
+    };
+    $scope.blueClicked = function() {
+        ActionService.performAction('#', 'B', '1', '1', 'B', '0', '0');
+    };
+    $scope.whiteClicked = function() {
+        ActionService.performAction('#', 'W', '1', '1', 'W', '0', '0');
     };
 }])
 ;
